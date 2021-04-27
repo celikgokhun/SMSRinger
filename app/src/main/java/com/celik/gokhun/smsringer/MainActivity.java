afdf5a9574dab62a240436ac0e7d35e5d5437b6a
celikgokhun <36249138+celikgokhun@users.noreply.github.com>
2021-04-28 02:34:41 +0300

    Ringer for Folks

--- app/src/main/java/com/celik/gokhun/smsringer/MainActivity.java
+++ app/src/main/java/com/celik/gokhun/smsringer/MainActivity.java
@@ -1,14 +1,140 @@
 package com.celik.gokhun.smsringer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.SmsManager;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String phoneNumberTaken;
+    String messageTaken;
+
+    EditText phoneNumberEditText;
+    EditText messageEditText;
+
+    static final int PICK_CONTACT=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+        messageEditText = findViewById(R.id.messageEditText);
+
     }
+
+    public void sendMessageViaContact(View view)
+    {
+        messageTaken = messageEditText.getText().toString();
+        getContactsNumber();
+        sendSMS(phoneNumberTaken,messageTaken);
+    }
+
+    public void sendText(View view)
+    {
+        messageTaken = messageEditText.getText().toString();
+        phoneNumberTaken = phoneNumberEditText.getText().toString();
+        sendSMS(phoneNumberTaken,messageTaken);
+    }
+
+    public void getContactsNumber()
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, PICK_CONTACT);
+        }
+
+        else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 10);
+            }
+        }
+    }
+
+    public void sendSMS(String phoneNumber, String message) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED)
+        {
+            try
+            {
+                SmsManager smsMgrVar = SmsManager.getDefault();
+                smsMgrVar.sendTextMessage(phoneNumber, null, message, null, null);
+                Toast.makeText(getApplicationContext(), "Message Sent",
+                        Toast.LENGTH_LONG).show();
+            }
+            catch (Exception ErrVar)
+            {
+                Toast.makeText(getApplicationContext(),ErrVar.getMessage().toString(),
+                        Toast.LENGTH_LONG).show();
+                ErrVar.printStackTrace();
+            }
+        }
+        else
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 10);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        switch (reqCode)
+        {
+            case (PICK_CONTACT) :
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Cursor c =  managedQuery(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+
+
+                        String id =c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+
+                        String hasPhone =c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                        if (hasPhone.equalsIgnoreCase("1"))
+                        {
+                            Cursor phones = getContentResolver().query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,
+                                    null, null);
+                            phones.moveToFirst();
+
+                            phoneNumberTaken = phones.getString(phones.getColumnIndex("data1"));
+                            phoneNumberEditText.setText(phoneNumberTaken);
+                            System.out.println("number is:"+phoneNumberTaken);
+
+                        }
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+
+                    }
+                }
+                break;
+        }
+    }
+
 }
